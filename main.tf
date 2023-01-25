355b0302674b65d119e96abd26d51b22969bae3c
dupegui334 <davidupegui334@hotmail.com>
2023-01-24 20:30:03 -0500

    added kms key to the bucket

--- main.tf
+++ main.tf
@@ -26,4 +26,18 @@ resource "aws_s3_bucket" "tf-backend-cron-server" {
     bucket = var.bucket_name
     acl = var.acl
     tags = var.tags
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
+
+resource "aws_kms_key" "mykey" {
+  description             = "Key for encrypt tfstate"
+  deletion_window_in_days = 10
+}
+
