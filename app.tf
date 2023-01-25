933789f58eb074bbbca0fdb6d30022baee5982c1
dupegui334 <davidupegui334@hotmail.com>
2023-01-24 22:05:02 -0500

    Added module app

---
+++ app.tf
@@ -0,0 +1,10 @@
+module "app" {
+  source = "./modules/instance"
+  ami_id = var.ami_id
+  instance_type = var.instance_type
+  tags = var.tags
+  sg_name = var.sg_name
+  ingress_rules = var.ingress_rules
+  bucket_name = var.bucket_name
+  acl = var.acl
+}
